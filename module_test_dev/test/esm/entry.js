94bc58729361e420db5541544e0a9aef13c3cf1f
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-06 17:00:48 +0800

    webpack - 1

--- module_test_dev/test/esm/entry.js
+++ module_test_dev/test/esm/entry.js
@@ -1,4 +1,5 @@
 import { counter, incA, foo } from './test.js';
+// import * as mod from './test.js';
 
 console.log(counter)
 // console.log(++counter)  TypeError: Assignment to constant variable
@@ -7,3 +8,4 @@ console.log(counter)
 
 console.log(foo);
 setTimeout(() => console.log(foo), 500);
+console.log(this);
