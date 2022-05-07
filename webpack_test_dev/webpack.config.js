c2ad7435c87d6471715913cf1de22423985e2ab0
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-07 16:27:49 +0800

    style-loader analysis

--- webpack_test_dev/webpack.config.js
+++ webpack_test_dev/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    
+    devtool: 'source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
