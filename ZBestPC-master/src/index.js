f71c7d0f80188157fa38afe8089464ec42c9c7fb
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-10 13:11:12 +0800

    tree-shaking intro

--- ZBestPC-master/src/index.js
+++ ZBestPC-master/src/index.js
@@ -4,3 +4,11 @@ import './css/index.css';
 import 'jquery';
 import './js/public';
 import './js/nav';
+
+// treeshaking触发条件
+// 1. 通过解构获取方法，可以触发treeshaking
+// 2. 调用的npm包必须使用ESM规范  ！！
+import { get } from 'lodash-es';
+
+// import _ from 'lodash';
+// console.log(get({ a: 1 }, 'a'));
