b2344c432a01c3693e8cee43d26ec5ee1ba23bd1
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-11 19:00:49 +0800

    webpack config init

--- ZBestPC-master/build/webpack.vue.config.js
+++ ZBestPC-master/build/webpack.vue.config.js
@@ -1,1 +1,77 @@
-module.exports
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const config = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, '../src/main.js')
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, '../dist')
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.(png|svg|gif|jpg|jpeg)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 8 * 1024
+                    }
+                },
+                generator: {
+                    filename: 'images/[name].[hash:6][ext]',
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: { loader: 'vue-loader' }
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        usedExports: true,
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            minSize: 30 * 1024,
+            cacheGroups: {
+
+            }
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html'),
+            filename: 'index.html',
+            chunks: ['index']
+        })
+    ]
+};
+
+module.exports = config;
