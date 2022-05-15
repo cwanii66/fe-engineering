65addfdac9cdc25658d3b1c696ead83544b6da59
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-15 10:55:11 +0800

    express static server

--- code_test/express-test/app.js
+++ code_test/express-test/app.js
@@ -1,26 +1,29 @@
 const express = require('express')
-const https = require('https')
-const fs = require('fs')
+// const https = require('https')
+// const fs = require('fs')
 
 const app = express()
 const PORT = 8000
 
-const httpsPort = 443
+// const httpsPort = 443
 
-const options = {
-    key: {
-        key: fs.readFileSync('./https/'), // 私钥
-        cert: fs.readFileSync('./https/') // 公钥
-    }
-}
+// const options = {
+//     key: {
+//         key: fs.readFileSync('./https/'), // 私钥
+//         cert: fs.readFileSync('./https/') // 公钥
+//     }
+// }
 
+// 同构部署 前端和后端的代码同构在一起的实现
+app.use('/static', express.static('./static'))
 
 app.use(function(req, res, next) {
-    res.send('Express server middleware..')
+    // res.send('Express server middleware..')
     console.log('step 1')
 
     throw new Error('error message!')
 })
+
 
 app.use(function(req, res, next) {
     console.log('step 2')
@@ -56,8 +59,8 @@ app.listen(PORT, function() {
     console.log(`express is listening on port ${PORT}`)
 })
 
-const httpsServer = https.createServer(options, app);
+// const httpsServer = https.createServer(options, app);
 
-httpsServer.listen(httpsPort, function() {
-    console.log('https 服务启动成功 -> Port: ' + httpsPort)
-})
+// httpsServer.listen(httpsPort, function() {
+//     console.log('https 服务启动成功 -> Port: ' + httpsPort)
+// })
