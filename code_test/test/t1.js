aaab4d4780c1744482c640531583cfbc23ae5387
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-20 19:54:01 +0800

    co module

--- code_test/test/t1.js
+++ code_test/test/t1.js
@@ -16,3 +16,27 @@ function createEventStream(heartbeat) {
         publish() {}
     }
 }
+
+function co(gen) {
+    const ctx = this;
+    
+    return new Promise(function(resolve, reject) {
+        if (typeof gen === 'function') gen = gen.call(ctx);
+        if (!gen || typeof gen.next !== 'function') return resolve(gen);
+
+        onFulfilled();
+        function onFulfilled(res) {
+            let ret;
+            try {
+                ret = gen.next(res);
+            } catch(e) {
+                return reject(e);
+            }
+            next(ret);
+        }
+
+        function next(ret) {
+            // call next itself recursively
+        }
+    });
+}
