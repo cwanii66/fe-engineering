e3fd30b7abcbeb7914934f0024740d146d100d28
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-26 21:05:29 +0800

    webpack5 vs 4

--- webpack-new-feature/demo-1/webpack4/webpack.config.js
+++ webpack-new-feature/demo-1/webpack4/webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'production',
     entry: {
         index: path.resolve(__dirname, 'src/index.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
+        filename: 'js/[name].bundle.js'
     },
     module: {
         rules: [
+            {},
+            {},
             {}
         ]
     },
---
+++ webpack-new-feature/demo-1/webpack5/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+const config = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    }
+}
+
+module.exports = config
