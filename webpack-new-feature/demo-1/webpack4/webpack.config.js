ee58f95951051538f4197d461ac94f25d60a6038
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-26 20:32:10 +0800

    webpack4 vs 5 init

---
+++ webpack-new-feature/demo-1/webpack4/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {}
+        ]
+    },
+    plugins: [
+
+    ]
+}
