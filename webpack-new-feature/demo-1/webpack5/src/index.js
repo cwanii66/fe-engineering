bb56f521456ecfc45eb19ca6f1d13e6f8a88e0e1
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-27 17:53:26 +0800

    webpack 5 assets handler

--- webpack-new-feature/demo-1/webpack5/src/index.js
+++ webpack-new-feature/demo-1/webpack5/src/index.js
@@ -1,9 +1,14 @@
 import o from './moduleA'
 import { ref } from 'vue'
 
+const imgUrl = require('./assets/voucher.png')
+const imgNode = document.createElement('img')
+imgNode.setAttribute('src', imgUrl)
+document.body.appendChild(imgNode)
+
 console.log('hello webpack5')
 const reactVal = ref({})
-console.log(reactVal)
+console.log(reactVal.value)
 const obj = { c: 3, d: 4 }
 console.log(o.a)
 export { obj as default }
--- webpack-new-feature/demo-1/webpack5/webpack.config.js
+++ webpack-new-feature/demo-1/webpack5/webpack.config.js
@@ -17,6 +17,22 @@ const config = {
     cache: {
         type: 'filesystem',
         cacheDirectory: path.resolve(__dirname, 'node_modules', '.cache_temp')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|jpeg|png|gif|webp|svg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 1024,
+                    }
+                },
+                generator: {
+                    filename: 'images/[name][hash:5].[ext]'
+                }
+            }
+        ]
     }
 }
 
