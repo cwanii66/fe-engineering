c96e15e9c8570e9f8c8201186ea15943abcf563c
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-28 16:33:37 +0800

    simple sideEffects config (boolean)

--- webpack-new-feature/demo-1/webpack4/src/index.js
+++ webpack-new-feature/demo-1/webpack4/src/index.js
@@ -1,17 +1,21 @@
-console.log('Hello webpack4')
+// console.log('Hello webpack4')
 
-import o from './moduleA'
-import { createApp } from 'vue'
+// import o from './moduleA'
+// import { createApp } from 'vue'
 
-const imgUrl = require('./assets/voucher.png')
+// const imgUrl = require('./assets/voucher.png')
 
-const imgNode = document.createElement('img')
-imgNode.src = imgUrl
-document.body.appendChild(imgNode)
+// const imgNode = document.createElement('img')
+// imgNode.src = imgUrl
+// document.body.appendChild(imgNode)
 
-createApp({})
-console.log(o.a)
+// createApp({})
+// console.log(o.a)
 
-export default {
-    b: 2
-}
+// export default {
+//     b: 2
+// }
+
+import './lib'
+// import 'vue'  // 模块的副作用难以衡量
+console.log(1)
--- webpack-new-feature/demo-1/webpack5/src/index.js
+++ webpack-new-feature/demo-1/webpack5/src/index.js
@@ -1,33 +1,37 @@
-import o from './moduleA'
-import { ref } from 'vue'
+// import o from './moduleA'
+// import { ref } from 'vue'
 
-// data:    protocol
-import data from 'data:text/javascript, export default "hello webpack5"'
-import func from 'data:text/javascript, export default function () { return { a: 1 } }'
-console.log(data)
-console.log(func().a)
+// // data:    protocol
+// import data from 'data:text/javascript, export default "hello webpack5"'
+// import func from 'data:text/javascript, export default function () { return { a: 1 } }'
+// console.log(data)
+// console.log(func().a)
 
-// file:    protocol
-import shield from 'file:///Users/chriswong/Desktop/LocalRepo/PersonalRepo/fe-engineering/webpack-new-feature/demo-1/img/jifen.jpeg'
-console.log(shield)
+// // file:    protocol
+// import shield from 'file:///Users/chriswong/Desktop/LocalRepo/PersonalRepo/fe-engineering/webpack-new-feature/demo-1/img/jifen.jpeg'
+// console.log(shield)
 
-// https&https:   protocol
-import moduleA from 'http://imooc-dev.youbaobao.xyz/test/moduleA.js'
-console.log(moduleA.a)
+// // https&https:   protocol
+// import moduleA from 'http://imooc-dev.youbaobao.xyz/test/moduleA.js'
+// console.log(moduleA.a)
 
-import moduleB from 'https://fast-learn-oss.youbaobao.xyz/tb/category6.png'
-const imgNodeB = document.createElement('img')
-imgNodeB.setAttribute('src', moduleB)
-document.body.appendChild(imgNodeB)
+// import moduleB from 'https://fast-learn-oss.youbaobao.xyz/tb/category6.png'
+// const imgNodeB = document.createElement('img')
+// imgNodeB.setAttribute('src', moduleB)
+// document.body.appendChild(imgNodeB)
 
-const imgUrl = require('./assets/voucher.png')
-const imgNode = document.createElement('img')
-imgNode.setAttribute('src', imgUrl)
-document.body.appendChild(imgNode)
+// const imgUrl = require('./assets/voucher.png')
+// const imgNode = document.createElement('img')
+// imgNode.setAttribute('src', imgUrl)
+// document.body.appendChild(imgNode)
 
-console.log('hello webpack5')
-const reactVal = ref({})
-console.log(reactVal.value)
-const obj = { c: 3, d: 4 }
-console.log(o.a)
-export { obj as default }
+// console.log('hello webpack5')
+// const reactVal = ref({})
+// console.log(reactVal.value)
+// const obj = { c: 3, d: 4 }
+// console.log(o.a)
+// export { obj as default }
+
+import './lib'
+import 'vue' // if no sideEffect (sideEffects: false), corresponding module will be deserted
+console.log(1)
