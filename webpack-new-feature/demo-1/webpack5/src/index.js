5a705cce5e4fd60dd842423ff624bd72b8b6c973
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-28 10:47:42 +0800

    webpack5 file:   import

--- webpack-new-feature/demo-1/webpack5/src/index.js
+++ webpack-new-feature/demo-1/webpack5/src/index.js
@@ -1,10 +1,15 @@
 import o from './moduleA'
 import { ref } from 'vue'
 
+// data:    protocol
 import data from 'data:text/javascript, export default "hello webpack5"'
 import func from 'data:text/javascript, export default function () { return { a: 1 } }'
 console.log(data)
 console.log(func().a)
+
+// file:    protocol
+import shield from 'file:///Users/chriswong/Desktop/LocalRepo/PersonalRepo/fe-engineering/webpack-new-feature/demo-1/img/jifen.jpeg'
+console.log(shield)
 
 const imgUrl = require('./assets/voucher.png')
 const imgNode = document.createElement('img')
