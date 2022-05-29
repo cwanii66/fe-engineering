a724b442b74e6f1ac0259c2db5f2bf07116231f5
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-29 18:10:32 +0800

    webpack5 Package Exports

---
+++ webpack-new-feature/demo-3/work/index.js
@@ -0,0 +1,5 @@
+// const library = require('lib')
+
+import library from 'lib'
+
+library()
---
+++ webpack-new-feature/demo-3/work/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+const config = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    optimization: {
+        splitChunks: {
+            minSize: 1 * 1024,
+            chunks: 'all',
+            name: 'vue'
+        }
+    },
+    cache: {
+        type: 'filesystem',
+        cacheDirectory: path.resolve(__dirname, 'node_modules', '.cache_temp')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|jpeg|png|gif|webp|svg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 1024,
+                    }
+                },
+                generator: {
+                    filename: 'images/[name][hash:5][ext]'
+                }
+            }
+        ]
+    },
+    experiments: {
+        buildHttp: {
+            allowedUris: [
+                'https://fast-learn-oss.youbaobao.xyz/',
+                'http://imooc-dev.youbaobao.xyz',
+            ],
+            frozen: false,
+            cacheLocation: false,
+            upgrade: true
+        },
+    }
+}
+
+module.exports = config
