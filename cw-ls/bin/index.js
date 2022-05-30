043732395e8a0692d99b5c6cbe30d73d3082c903
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-30 15:57:30 +0800

    simple -a -l

--- cw-ls/bin/index.js
+++ cw-ls/bin/index.js
@@ -8,12 +8,22 @@ const args = process.argv.slice(2)
 
 const { isAll, isList } = parse(args)
 
-// pure ls mode
-if (!isAll && !isList) {
-    // 遍历当前文件夹下的所有文件，并剔除以 . 开头的文件或文件夹
-    let files = fs.readdirSync(dir)
+let files = fs.readdirSync(dir)
+let output = ''
+
+if (!isAll) {
     files = files.filter(file => file.indexOf('.') !== 0)
-    let output = ''
-    files.forEach(file => output += file + '\t' )
-    console.log(output)
 }
+
+if (!isList) {
+    // 遍历当前文件夹下的所有文件，并剔除以 . 开头的文件或文件夹
+    files.forEach(file => output += file + '\t')
+} else {
+    files.forEach((file, index, files) => {
+        if (index === files.length - 1) {
+            output += file
+        }
+        output += file + '\n'
+    })
+}
+console.log(output)
