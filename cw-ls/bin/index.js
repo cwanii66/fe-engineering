f3b3993fb2a0fd3f3d3c3d83bf71af10fa9ada6a
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-30 15:24:43 +0800

    iterate fs

--- cw-ls/bin/index.js
+++ cw-ls/bin/index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
 const parse = require('./argsParser.js')
 
+const dir = process.cwd()
 const args = process.argv.slice(2)
 
 const { isAll, isList } = parse(args)
 
-console.log(args, isAll, isList)
+// pure ls mode
+if (!isAll && !isList) {
+    // 遍历当前文件夹下的所有文件，并剔除以 . 开头的文件或文件夹
+    let files = fs.readdirSync(dir)
+    files = files.filter(file => file.indexOf('.') !== 0)
+    let output = ''
+    files.forEach(file => output += file + '\t' )
+    console.log(output)
+}
