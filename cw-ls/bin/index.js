6957e74f599e0c305c566ac7fc4711fcc8885d5b
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-31 22:13:24 +0800

    file mode

--- cw-ls/bin/index.js
+++ cw-ls/bin/index.js
@@ -28,8 +28,23 @@ if (!isList) {
     // Unix使用32位二进制数存储文件类型和权限
     // 0000 文件类型、 000 特殊权限、000 用户权限、 000 分组权限、 000其他权限
     // example: 0001: File   0100: Directory
+    // bin: 16877 .json: 33188
+
+    // bin: 0100 0001 1110 1101
+    // S_IFDIR: 0100 0000 0000 0000
+
     files.forEach((file, index, files) => {
-        const mode = fs.statSync(file) // 拿到文件mode编码
+        // const stat = fs.statSync(file) // 拿到文件mode编码
+        // const mode = stat.mode
+        // const isDir = mode & fs.constants.S_IFDIR  // mode跟相应4位二进制文件类型进行与操作
+        // const isFile = mode & fs.constants.S_IFREG
+        // console.log(isFile > 0)
+        // console.log(isDir > 0) // 正结果isDir
+        // console.log(
+        //     mode, 
+        //     'isDirectory:', stat.isDirectory(), // isDir > 0
+        //     'isFile:', stat.isFile(), // isFile > 0
+        // )
         if (index === files.length - 1) {
             return output += file
         }
