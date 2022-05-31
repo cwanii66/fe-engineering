9d7308b6064a61d47986b66812f39fb781e2fc7f
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-31 21:51:48 +0800

    unix file system

--- cw-ls/bin/index.js
+++ cw-ls/bin/index.js
@@ -19,9 +19,19 @@ if (!isList) {
     // 遍历当前文件夹下的所有文件，并剔除以 . 开头的文件或文件夹
     files.forEach(file => output += file + '\t')
 } else {
+    // Unix (linux macos) 文件权限体系
+    // bin：rwx r-x r-x
+    // package.json: rw- r-- r--
+    // r: 访问、w：编辑、x：执行
+    // u: 当前登录用户，g：当前登录用户所在分组，o：其他用户
+    // node.js 如何获取文件权限？
+    // Unix使用32位二进制数存储文件类型和权限
+    // 0000 文件类型、 000 特殊权限、000 用户权限、 000 分组权限、 000其他权限
+    // example: 0001: File   0100: Directory
     files.forEach((file, index, files) => {
+        const mode = fs.statSync(file) // 拿到文件mode编码
         if (index === files.length - 1) {
-            output += file
+            return output += file
         }
         output += file + '\n'
     })
