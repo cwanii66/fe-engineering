dc7c3925485b523686b6c64571892ee382837023
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-06-03 11:32:29 +0800

    split( separator, limit)

--- cw-build/bin/cw-build.js
+++ cw-build/bin/cw-build.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 
+const { program } = require('commander')
+
+program
+    .option('--first')
+    .option('-s, --separator <char>')
+    .option('-h, --help')
+
+program.parse()
+
+const options = program.opts()
+const limit = options.first ? 1 : undefined // 代表分割参数的个数
+// cw-build aaa/bbb/cc --first -s /
+
+console.log(program.args[0].split(options.separator, limit))
