7ca9f22079b58c1a852672890976a39e48ee12d4
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-06-05 11:24:10 +0800

    how to generate docs ?

--- cw-build/bin/cw-build.js
+++ cw-build/bin/cw-build.js
@@ -3,6 +3,9 @@
 const { Command } = require('commander')
 const program = new Command()
 
+// 1. 生成脚手架的帮助文档： cw-build -h
+// 2. 生成脚手架command的帮助文档： cw-build split -h / cw-build help split
+
 program
     .name('cw-build')
     .description('CLI to build fe project')
