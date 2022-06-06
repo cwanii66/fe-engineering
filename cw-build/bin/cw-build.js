d37f6c0bacec4922c2c7933d7554727f17ef9c80
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-06-06 11:25:00 +0800

    get scoped & global options

--- cw-build/bin/cw-build.js
+++ cw-build/bin/cw-build.js
@@ -12,14 +12,37 @@ program
     .version('0.0.1')
 
 program
+    .option('-d --debug', 'debugging')
+
+program
     .command('split')
     .description('Split string to array')
     .argument('<string>', 'string to split')
-    .option('--first', 'display just the first substring')
+    .option('--first', 'display just the first substring')  // options here are scoped at this command
     .option('-s, --separator <char>', 'separator character', ',')
-    .action((str, options) => {
+    .option('-e, --extra', 'extra for something')
+    .option('-a --add [string]', 'add something')
+    .action((str, options, cmd) => {
+        console.log(options)
+        // console.log(program.commands[0].optsWithGlobals())
+        // console.log(program.getOptionValue('debug')) // get optionValue globally
+        console.log(cmd.optsWithGlobals())
+        console.log(cmd.opts())
         const limit = options.first ? 1 : undefined
         console.log(str.split(options.separator, limit))
     })
 
 program.parse()
+
+// opts: 获取当前十里的options，全局program获取全局options，subcommand获取局部options
+// optsWithGlobals: 获取全部options，全局program获取全局options，但是subcommand获取 ** 全局+局部options **
+const options = program.opts() // global options
+const globalOptions = program.globalOptions
+// console.log(globalOptions)
+// console.log(options)
+
+// options 四种调用方式
+// -s <char>
+// -s<char>
+// --separator <char>
+// --separator=<char>
