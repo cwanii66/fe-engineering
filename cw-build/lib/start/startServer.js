3ef3291a5c7528179de8b6b69d8b6d96acc1a642
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-06-12 11:32:49 +0800

    chokidar watch fs

--- cw-build/lib/start/startServer.js
+++ cw-build/lib/start/startServer.js
@@ -1,5 +1,33 @@
+const chokidar = require('chokidar');
+const path = require('path');
 
+function runServer() {
+    // 启动webpack服务
+
+}
+
+function runWatcher() {
+    // 启动配置监听
+    const configPath = path.resolve('./config.json');
+    const startPath = path.resolve(process.cwd(), 'lib/start');
+    const buildPath = path.resolve(process.cwd(), 'lib/build');
+    const watcher = chokidar.watch(startPath)
+        .on('all', (eventName, path) => {
+            console.log(eventName, path)
+            // console.log(watcher.getWatched())
+        });
+    watcher.add(startPath);
+    watcher.add(buildPath);
+    watcher.close()
+        .then(() => { console.log('closed') });
+}
 
 module.exports = function startServer(args, opts, cmd) {
-    console.log('start server...')
-}
+    // 1. 通过子进程启动webpack-dev-server服务
+    // 1.1 子进程启动可以避免主进程收到影响
+    // 1.2 子进程启动可以方便重启，解决webpack-dev-server配置修改后无法重启
+    runServer();
+
+    // 2. 监听配置修改(不管用webpack还是vite最终都会映射到一个配置文件上)
+    runWatcher();
+};
