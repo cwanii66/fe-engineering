115e8ce549385f921555623e65bc0957f5dbf59c
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-14 20:55:01 +0800

    vite plugin

--- vite-test/vite.config.js
+++ vite-test/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue';
-
+import visualizer from "rollup-plugin-visualizer";
 export default defineConfig({
     base: '/test/',
     plugins: [
-        vue()
+        vue(),
+        {
+            ...visualizer(),
+            enforce: 'pre',
+            apply: 'build'
+        },
     ],
 });
