88400e792828198394eea697f64e606a222018d8
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-14 16:49:57 +0800

    vite for vue

---
+++ vite-test/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>breaking bad</h1>
+    <div class="test">better call saul</div>
+  </div>
+</template>
+
+<script setup>
+import utils from './utils';
+import './index.scss';
+
+console.log('vite test...');
+utils();
+
+</script>
--- vite-test/vite.config.js
+++ vite-test/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     base: '/test/',
+    plugins: [
+        vue()
+    ],
 });
