7e0d92c90ba46c1e4c52fcf044057d892df7e645
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-18 20:59:28 +0800

    custom loader execution

--- vue-loader/_loader/loader1.js
+++ vue-loader/_loader/loader1.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 exports.default = function(content) {
     const { resourcePath, resourceQuery = '' } = this;
     console.log('loader1', resourcePath, resourceQuery);
     if (resourceQuery === '?vue&type=template&id=123456') {
         return 'console.log(\'new code\');';
     } else {
-        content = `import '${resourcePath}?vue&type=template&id=123456';\n${content}`;
+        content = `import '-!${path.resolve(__dirname, 'loader1.js')}!${resourcePath}?vue&type=template&id=123456';\n${content}`;
     }
     console.log('loader1 content: ', content);
     return content;
